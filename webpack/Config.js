f6cbde6540ce9e2d1d329e78c4c7d384a20fb67f
Vasil Nedev <vasilnedev55@gmail.com>
2020-05-23 13:21:05 +0100

    Remove debug from webpack Config

--- webpack/Config.js
+++ webpack/Config.js
@@ -1,7 +1,6 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 
 export default {
-	debug : true,
 	devtool : 'inline-source-map',
 	entry : {
 		vendor : './src/vendor',
@@ -17,11 +16,14 @@ export default {
 			template : './src/index.html',
 			inject : true
 		})
-	],
+	]
+/*	
+	,
 	module: {
 		loaders: [
 			{test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
 			{test: /\.css/, loaders: ['style','css']}
 		]
 	}
+*/
 }
