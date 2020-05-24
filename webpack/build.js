67078e9abd222353dd29bf18ea09130f08cf340e
Vasil Nedev <vasilnedev55@gmail.com>
2020-05-24 09:32:43 +0100

    Refine README, package.json and build.js

--- webpack/build.js
+++ webpack/build.js
@@ -2,13 +2,15 @@ import webpack   from 'webpack'
 import wpConfig from './webpack.config'
 
 webpack( wpConfig , (err,stats) => {
-	if(err){
+	if( err ){
 		console.error( err )
 		return 1
 	}
-
 	const info = stats.toJson()
 	if( stats.hasErrors()   ) console.error( info.errors )
 	if( stats.hasWarnings() ) console.warn ( info.warnings )
-
+  if( !stats.hasErrors() && !stats.hasWarnings() ){
+    console.log( 'All looks OK!' )
+    return 0
+  }else{ return 1 }
 })
