76fe8c9f8ed0390c551fc82d7039533d277c75f9
Vasil Nedev <vasilnedev55@gmail.com>
2020-05-25 16:45:30 +0100

    Modify git-pull.js

--- tools/git-pull.js
+++ tools/git-pull.js
@@ -1,10 +1,15 @@
 import shell from 'shelljs'
 
-shell.exec( 'git pull' , ( code , stdout , stderr ) => {
+shell.exec('git status' , ( code , stdout , stderr ) => {
   if( code == 0 ){
-    if( stdout.includes('package.json') ){
-      return shell.exec(' npm prune && npm install && git stash && git stash clear ')
-    }else{ return 0 }
+    if( stdout.includes('package.json') ){ if( shell.exec('git stash && git stash clear ').code != 0) { return 1 } }
+    shell.exec('git pull' , ( code , stdout , stderr ) => {
+      if( code == 0 ){
+        if( stdout.includes('package.json') ){ return shell.exec('npm prune && npm install && git stash && git stash clear ').code }
+        return 0
+      }
+      return 1
+    })
   }
   return 1
 })
