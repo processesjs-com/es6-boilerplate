a0b7ba804887b8fef171db018b7e6f611350bd78
Vasil Nedev <vasilnedev55@gmail.com>
2020-05-26 13:57:01 +0100

    Cosmetics

--- tools/git-pull.js
+++ tools/git-pull.js
@@ -1,3 +1,11 @@
+/*
+    This script shall be used on the Bulid Server to pull from GitHub.
+    If there are changes in package.json, the script runs 'npm prune && npm install' to update the dependencies.
+    To ignore the local (on the Build Server) changes in package.json, the script runs 'git stash && git stash clear'.
+
+    Note: 'node_modules' and 'dist' folders; and package-lock.json are in .gitignore list
+*/
+
 import shell from 'shelljs'
 
 shell.exec('git status' , ( code , stdout , stderr ) => {
