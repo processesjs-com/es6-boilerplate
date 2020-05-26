946967a46016281485ead5b3d1838ea3ddeb2c73
Vasil Nedev <vasilnedev55@gmail.com>
2020-05-26 16:40:18 +0100

    Fix deploy-s3

--- tools/deploy-s3.js
+++ tools/deploy-s3.js
@@ -6,31 +6,32 @@ import gitBranch from 'current-git-branch'
 const pathToDist = path.resolve( __dirname , '../dist' )
 const Bucket = 'es6-boilerplate'
 
-if( gitBranch()!='master '){ console.log( 'Please deploy from master branch only.' ) ; return 1 }
+if( gitBranch()=='master' ){
 
-AWS.config.update({region: 'eu-central-1'})
-const s3 = new AWS.S3()
+  AWS.config.update({region: 'eu-central-1'})
+  const s3 = new AWS.S3()
 
-// Clear bucket
-s3.listObjects( { Bucket } , ( err , data ) => {
-  if (err) { console.log( "Error listing bucket objects: ", err ) ; return 1 }
-  for( const item of data.Contents ){
-    s3.deleteObject( { Bucket , Key: item.Key } , ( err , data ) => {
-      if ( err ) { console.log( "Delete err: ", err ) ; return 1 }
-      else { console.log("Deleted: ", item ) }
-    })
-  }
-})
-
-// Copy dist folder to the bucket
-fs.readdir( pathToDist , ( err , files ) => {
-    if( err ){ console.log('Unable to scan directory: ' + err) ; return 1 }
-    for( const file of files )
-    {
-      const uploadParams = { Bucket , Body: fs.createReadStream( pathToDist + '/' + file ) , Key: file }
-      s3.upload ( uploadParams , ( err , data ) => {
-        if ( err )  { console.log( "Upload error: " , err ) ; return 1 }
-        if ( data ) { console.log( "Uploaded : " , data.Location) }
+  // Clear bucket
+  s3.listObjects( { Bucket } , ( err , data ) => {
+    if (err) { console.log( "Error listing bucket objects: ", err ) ; return }
+    for( const item of data.Contents ){
+      s3.deleteObject( { Bucket , Key: item.Key } , ( err , data ) => {
+        if ( err ) { console.log( "Delete err: ", err ) ; return }
+        else { console.log("Deleted: ", item ) }
       })
     }
-})
+  })
+
+  // Copy dist folder to the bucket
+  fs.readdir( pathToDist , ( err , files ) => {
+      if( err ){ console.log('Unable to scan directory: ' + err) ; return }
+      for( const file of files )
+      {
+        const uploadParams = { Bucket , Body: fs.createReadStream( pathToDist + '/' + file ) , Key: file }
+        s3.upload ( uploadParams , ( err , data ) => {
+          if ( err )  { console.log( "Upload error: " , err ) ; return }
+          if ( data ) { console.log( "Uploaded : " , data.Location) }
+        })
+      }
+  })
+}else{ console.log( 'Please deploy from master branch only.' ) }
